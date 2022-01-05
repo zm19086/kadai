8a983568e8e6dcad9f51e605bc2e031f9b81694e
zm19086 <zm19086@josai.ac.jp>
2022-01-05 13:42:16 +0000

    i5

--- 2script.js
+++ 2script.js
@@ -1,22 +1,14 @@
-let elapsedTime = 180;
+let elapsedTime = 180 - keika;
 function wrightTime(){
     document.getElementById("time").textContent = '残り' + elapsedTime + '秒';
 }
 wrightTime();
 
-let keika = 0;
-function minus(){keika ++;}
-
 const timerId = setInterval(function(){
     elapsedTime--;
-    minus();
     wrightTime();
     if (elapsedTime === 0){
         clearInterval(timerId);
         alert('時間切れ!');alert(score + '問正解!');
     }
 },1000);
-
-
-
-
