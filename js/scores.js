d07d257461d3b7de5421e53bcfde1681bd0d069b
zm19086 <zm19086@josai.ac.jp>
2022-02-07 05:03:13 +0000

    ver.1.0.0

--- js/scores.js
+++ js/scores.js
@@ -1,22 +1,71 @@
-//　　制限時間を２１０秒（３分半）に設定
+// 持ち点の設定
 
-let elapsedTime = 210;
+var score = 0
 
-// 残り時間の表示
+// 正解すると持ち点に＋１する
 
-function wrightTime(){
-    document.getElementById("time").textContent = '残り' + elapsedTime + '秒';
+function add(){
+  score ++ ;
 }
-wrightTime();
 
-// setIntervalを使ったカウントダウンタイマー
+// 正解済みか未正解かの仕分け
 
-const timerId = setInterval(function(){
-    elapsedTime--;
-    wrightTime();
-    if (elapsedTime === 0){
-        clearInterval(timerId);
-        alert('時間切れ!');
-        next11();
-    }
-},1000);
+var Q1 = 0
+var Q2 = 0
+var Q3 = 0
+var Q4 = 0
+var Q5 = 0
+var Q6 = 0
+var Q7 = 0
+var Q8 = 0
+var Q9 = 0
+var Q10 = 0
+
+function add1(){
+  Q1 ++ ;
+}
+
+function add2(){
+  Q2 ++ ;
+}
+
+function add3(){
+  Q3 ++ ;
+}
+
+function add4(){
+  Q4 ++ ;
+}
+
+function add5(){
+  Q5 ++ ;
+}
+
+function add6(){
+  Q6 ++ ;
+}
+
+function add7(){
+  Q7 ++ ;
+}
+
+function add8(){
+  Q8 ++ ;
+}
+
+function add9(){
+  Q9 ++ ;
+}
+
+function add10(){
+  Q10 ++ ;
+}
+
+//結果を見るボタンが押されたら変数scoreを使って点数を表示する。
+
+function kekka(){
+  document.getElementById('content').innerHTML ='<p class="title2"><span class="iro1">' + score + '</span>問正解です!!</p>'+
+  '<div class="rtnbtn">'+
+  '<a href="index.html" class="btn btn--orange2">最初のページに戻る</a>'+
+  '</div>';
+}
